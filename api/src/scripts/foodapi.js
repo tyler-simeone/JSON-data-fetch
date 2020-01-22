dd9371baadae4c47c1bb9c601c22e6440f7613fa
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-22 11:51:35 -0600

    new factory fn, not working yet

--- api/src/scripts/foodapi.js
+++ api/src/scripts/foodapi.js
@@ -1,27 +1,58 @@
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        console.table(parsedFoods)
-    })
+// These functions are working, but it's hard-coded. 
+// Need to use a more dynamic fn... Look at the chapter.
 
-const createString = (name, ethnicity, type) => {
+// const createString = (name, ethnicity, type) => {
+//     return `
+//     <li>
+//         <h1>${name}</h1>
+//         <p>${ethnicity}</p>
+//         <p>${type}</p>
+//     </li>
+//     `
+// };
+
+// NEW TRY
+const foodFactory = (food) => {
     return `
-        <li>
-            <h1>${name}</h1>
-            <p>${ethnicity}</p>
-            <p>${type}</p>
-        </li>
+    <li>
+        <h1>${food.name}</h1>
+        <p>${food.ethnicity}</p>
+        <p>${food.type}</p>
+    </li>
     `
 };
 
+// const addStringToDom = (name, ethnicity, type) => {
+//     container.innerHTML += createString(name, ethnicity, type);
+// }
+
 const container = document.querySelector(".foodList");
 
-const addStringToDom = (name, ethnicity, type) => {
-    container.innerHTML += createString(name, ethnicity, type);
+// NEW TRY
+const addFoodToDom = (food) => {
+    container.innerHTML += foodFactory(food);
 }
 
-addStringToDom("Linguine", "italian", "pasta");
-addStringToDom("Massaman curry", "thai", "curry");
-addStringToDom("Green curry", "thai", "curry");
-addStringToDom("Ravioli Funghi", "italian", "pasta");
-addStringToDom("Tuscan Soup", "italian", "vegetarian");
+// NEW TRY
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food);
+            addFoodToDom(foodAsHTML);
+        })
+    })
+
+// fetch("http://localhost:8088/food")
+//     .then(foods => foods.json())
+//     .then(parsedFoods => {
+//         console.table(parsedFoods)
+//     })
+
+
+
+// addStringToDom("Linguine", "italian", "pasta");
+// addStringToDom("Massaman curry", "thai", "curry");
+// addStringToDom("Green curry", "thai", "curry");
+// addStringToDom("Ravioli Funghi", "italian", "pasta");
+// addStringToDom("Tuscan Soup", "italian", "vegetarian");
