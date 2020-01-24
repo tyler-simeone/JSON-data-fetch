d47dcbe3133516c97efad9a76c4fb715b0b1e64c
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-24 09:07:02 -0600

    working on nested fetch

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -6,7 +6,12 @@ const foodFactory = (food) => {
     <li>
         <h1>${food.name}</h1>
         <p>${food.ethnicity}</p>
-        <p>${food.category}</p>
+        <p>${food.type}</p>
+        <p>${food.ingredients}</p>
+        <p>${food.origin}</p>
+        <p>${food.calories}</p>
+        <p>${food.fats}</p>
+        <p>${food.sugars}</p>
     </li>
     `
 };
@@ -23,8 +28,20 @@ fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
         parsedFoods.forEach(food => {
-            const foodAsHTML = foodFactory(food);
-            addFoodToDom(foodAsHTML);
+            // const foodAsHTML = foodFactory(food);
+            // addFoodToDom(foodAsHTML);
+
+            // So this conditional works for ingredients, but how do I
+            // get the other four properties needed?
+            fetch("https://world.openfoodfacts.org/api/v0/product/${food}.json")
+                .then(resp => resp.json())
+                .then(productInfo => {
+                    if (productInfo.product.ingredients_text, if (productInfo.product.ingredients_text) {
+                        food.ingredients = productInfo.product.ingredients_text;
+                    } else {
+                        food.ingredients = "no ingredients listed";
+                    }
+                })
         })
     })
 
